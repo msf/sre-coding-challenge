15658c923952ace9aaff14f4450766decd9c8c0d
Miguel Filipe <miguel.filipe@gmail.com>
2019-04-16 14:50:54 +0100

    add nginx

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ docker build -t build-base -f Dockerfile-base .
 docker build -t multivac -f Dockerfile .
 docker build -t multivac-web -f Dockerfile-web .
 docker build -t multivac-worker -f Dockerfile-worker .
+docker build -t multivac-nginx -f Dockerfile-nginx .
+docker run -it multivac-nginx nginx -t
 
