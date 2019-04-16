a7bbbfe76bcab78a0828773181146ddfe35a833c
Miguel Filipe <miguel.filipe@gmail.com>
2019-04-16 09:42:14 +0100

    update docker files

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-docker build -t multivac-worker -f Dockerfile-worker .
+docker build -t build-base -f Dockerfile-base .
+docker build -t multivac -f Dockerfile .
 docker build -t multivac-web -f Dockerfile-web .
+docker build -t multivac-worker -f Dockerfile-worker .
 
